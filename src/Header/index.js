98299b8156efd218634d52c6daccf15d6b4c6125
Haridokku <124425442+Haridokku@users.noreply.github.com>
2024-02-18 21:36:29 +0530

    Update index.js

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,7 +1,33 @@
 import './index.css'
 
-const Header = () => {
-  return ()
-   
+const Header = props => {
+  const {score, seconds} = props
+  return (
+    <nav className="nav-container">
+      <ul>
+        <li>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/match-game-website-logo.png"
+            alt="website logo"
+            className="logo"
+          />
+        </li>
+      </ul>
+
+      <div className="score-card">
+        <p className="score">
+          Score:<span className="seconds">{score}</span>
+        </p>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/match-game-timer-img.png"
+          alt="timer"
+          className="time"
+        />
+        <p className="seconds">{seconds} sec</p>
+      </div>
+    </nav>
+  )
 }
+
 export default Header
+
